5aa11277aac2b23878b25ff9973dedb3034aab17
Anthony G <egiulia0@email.cpcc.edu>
2025-01-11 17:50:41 -0500

    Part 1 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    phrase = '';
 
+if (times < 0)
+{
+    phrase = 'ERROR';
+}
+
+    for (let i = 0; i < times; i++)
+    {
+        phrase += word;
+    }
+
+    return phrase;
 };
 
 // Do not edit below this line
