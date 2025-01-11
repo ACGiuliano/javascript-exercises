9637408ea4e7e72230007fc1f907785a22b67041
Anthony G <egiulia0@email.cpcc.edu>
2025-01-11 18:07:25 -0500

    Part 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,29 @@
-const sumAll = function() {
+const sumAll = function(start, finish) {
+    if (typeof start !== 'number' || typeof finish !== 'number') {
+        return 'ERROR';
+    }
+
+    if (!Number.isInteger(start) || !Number.isInteger(finish)) {
+        return 'ERROR';
+    }
+
+    if (start < 0 || finish < 0) {
+        return 'ERROR';
+    }
+
+    if (start > finish) {
+        const temp = start;
+        start = finish;
+        finish = temp;
+    }
+
+    let sum = 0;
+    for (let i = start; i <= finish; i++)
+    {
+        sum += i;
+    }
+    
+    return sum;
 
 };
 
