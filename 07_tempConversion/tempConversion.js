706184578f45cab5d83bd3246db239ae5f65d1da
Anthony G <egiulia0@email.cpcc.edu>
2025-01-11 18:19:07 -0500

    Part 7 Completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let celsius = (farenheit - 32) * 5 / 9;
+  rounded = Math.round(celsius * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let farenheit = celcius * 9 / 5 + 32;
+  rounded = Math.round(farenheit * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
